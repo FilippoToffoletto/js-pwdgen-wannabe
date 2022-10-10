e53ee4988c55091cbd48de28880a71f60fc6fb30
FilippoToffoletto <filippotoffoletto@hotmail.it>
2022-10-10 14:54:30 +0200

    first

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Chiedi all’utente il suo nome,
+// poi chiedi il suo cognome,
+// poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina nomecognomecolorepreferito22
+
+const userFirstName = prompt('Come ti chiami?');
+const userLastName = prompt('Come ti chiami?');
