ff2d4f9928501842594c7affd4ddc6d082f144a2
FilippoToffoletto <filippotoffoletto@hotmail.it>
2022-10-10 15:05:13 +0200

    final

--- js/script.js
+++ js/script.js
@@ -3,5 +3,14 @@
 // poi chiedi il suo colore preferito
 // Infine scrivi sulla pagina nomecognomecolorepreferito22
 
-const userFirstName = prompt('Come ti chiami?');
-const userLastName = prompt('Come ti chiami?');
+const userFirstName = prompt('First Name?');
+const userLastName = prompt('Last name?');
+const userPreferColor = prompt('Prefer color?');
+const userPreferNumber = prompt('Prefer number?');
+
+output = `
+    <strong>La tua password è</strong><br>
+    ${userFirstName}${userLastName}${userPreferColor}${userPreferNumber}.
+`;
+
+document.getElementById('password').innerHTML += output;
